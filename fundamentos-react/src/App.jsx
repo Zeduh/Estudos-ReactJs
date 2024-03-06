bf84cffc460f7f055fe6ab9a9b6b0c91ad37a684
zeduh <jeduardocury@gmail.com>
2024-03-06 09:00:15 -0300

    Versão final da aula de React

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 import React from "react";
 
-import Primeiro from './components/Primeiro'
-import ComParametro from './components/ComParametro'
-import ComFilhos from './components/ComFilhos'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
 import Card from './components/layout/Card'
+import Repeticao from './components/basicos/repeticão'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
 
 export default props =>
     <div className="App">
+        <Card titulo="#06 - Condicional V2">
+            <CondicionalComIf numero={10} />
+        </Card>
+
+        <Card titulo="#05 - Condicional V1">
+            <Condicional numero={10} />
+        </Card>
+
+        <Card titulo="#04 - Repetição">
+            <Repeticao />
+        </Card>
+
         <Card titulo="#03 - Componente com Filhos">
             <ComFilhos>
                 <ul>
---
+++ fundamentos-react/src/data/products.js
@@ -0,0 +1,5 @@
+export default [
+    { id:1, nome: 'caneta', preco: 7.50},
+    { id:2, nome: 'lapis', preco: 2.50},
+    { id:3, nome: 'caderno', preco: 15}
+]
